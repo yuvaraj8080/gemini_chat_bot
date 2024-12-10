4fd212e5a9f7decef51a35ee2adf2000ab2b2779
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2024-12-11 00:04:45 +0530

    this the my first gemini ai app

--- lib/repositories/chat_repository.dart
+++ lib/repositories/chat_repository.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:google_generative_ai/google_generative_ai.dart';
